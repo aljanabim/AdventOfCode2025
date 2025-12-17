faec1db857532ed9225c9bdcf049427bb9182a9f
aljanabim <ma970101@yahoo.se>
2025-12-18 00:48:53 +0100

    Solve day 11 part 1

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -51,4 +51,4 @@ var day1Solver = new Puzzles.Day1.Solution();
 
 var day11Solver = new Puzzles.Day11.Solution("Data/day11_input.txt");
 day11Solver.Solve(11, 1);
-day11Solver.Solve(11, 2);
+// day11Solver.Solve(11, 2);
--- src/Puzzles/Day11/Solution.cs
+++ src/Puzzles/Day11/Solution.cs
@@ -1,11 +1,42 @@
 namespace Puzzles.Day11;
 
-public class Solution(string inputFileName) : SolutionBase<int>(inputFileName)
-
+public enum NodeType
 {
+    You,
+    Out,
+    Other
+}
+
+internal class Node
+{
+    internal required string Id;
+    internal Node[] Outputs = [];
+    internal NodeType Type = NodeType.Other;
+};
+
+public class Solution(string inputFileName) : SolutionBase<int>(inputFileName)
+{
+    readonly Dictionary<string, Node> Nodes = [];
+
     public override int SolvePart1()
     {
-        throw new NotImplementedException();
+
+        int outCounter = 0;
+        var queue = new Queue<Node>();
+        queue.Enqueue(Nodes["you"]);
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            if (node.Type == NodeType.Out)
+                outCounter++;
+            foreach (var output in node.Outputs)
+                queue.Enqueue(output);
+        }
+        // foreach (var node in Nodes.Values)
+        // {
+        //     Console.WriteLine($"{node.Id}: {string.Join("-", node.Outputs.Select(n => n.Id))} {node.Type}");
+        // }
+        return outCounter;
     }
 
     public override int SolvePart2()
@@ -15,6 +46,28 @@ public class Solution(string inputFileName) : SolutionBase<int>(inputFileName)
 
     internal override void ParseInput()
     {
-        throw new NotImplementedException();
+        // Populate nodes
+        for (int i = 0; i < Input.Length; i++)
+        {
+            var nodeId = Input[i].Split(":").First();
+            Nodes[nodeId] = new Node
+            {
+                Id = nodeId,
+                Outputs = [],
+                Type = nodeId switch
+                {
+                    "you" => NodeType.You,
+                    "out" => NodeType.Out,
+                    _ => NodeType.Other
+                }
+            };
+        }
+        Nodes["out"] = new Node { Id = "out", Outputs = [], Type = NodeType.Out };
+        // Populate output connections foreach node
+        for (int i = 0; i < Input.Length; i++)
+        {
+            var nodeId = Input[i].Split(':', StringSplitOptions.RemoveEmptyEntries).First();
+            Nodes[nodeId].Outputs = [.. Input[i].Split(':').Last().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(n => Nodes[n])];
+        }
     }
 }
