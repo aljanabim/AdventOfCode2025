87e373ba21070f97e0d16f91859ebef20eba6a16
aljanabim <ma970101@yahoo.se>
2025-12-19 23:34:04 +0100

    Finish solving day 11 part 2

--- src/Puzzles/Day11/Solution.cs
+++ src/Puzzles/Day11/Solution.cs
@@ -40,78 +40,45 @@ public class Solution(string inputFileName) : SolutionBase<ulong>(inputFileName)
     }
 
 
-    Dictionary<string, int> finderCache = [];
-    internal int PathFinder(Node node, bool fftVisited, bool dacVisited)
+    internal record NodeCheckPoint(ulong Visits, ulong DacVisits, ulong FftVisits);
+    readonly Dictionary<string, NodeCheckPoint> finderCache = [];
+    internal NodeCheckPoint PathFinder(Node node)
     {
         if (node.Type == NodeType.Out)
-            return fftVisited && dacVisited ? 1 : 0;
-        else if (node.Outputs.Length == 0)
-            return 0;
+        {
+            return new NodeCheckPoint(1, 0, 0);
+        }
+        if (node.Outputs.Length == 0)
+            return new NodeCheckPoint(0, 0, 0);
 
-        if (finderCache.TryGetValue(node.Id, out int counter))
-            return counter;
-        counter = 0;
-        bool visitingFFT = node.Id == "fft" || fftVisited;
-        bool visitingDAC = node.Id == "dac" || dacVisited;
-
+        if (finderCache.TryGetValue(node.Id, out var checkPoint))
+        {
+            return checkPoint;
+        }
+        ulong visits = 0;
+        ulong dacVisits = 0;
+        ulong fftVisits = 0;
         foreach (var output in node.Outputs)
         {
-            var pathCount = PathFinder(output, visitingFFT, visitingDAC);
-            Console.WriteLine($"Node {node.Id} Output {output.Id} pathCount {pathCount} fft={visitingFFT} dac={visitingDAC}");
-
-
-            counter += pathCount;
+            var point = PathFinder(output);
+            visits += point.Visits;
+            dacVisits += point.DacVisits;
+            fftVisits += point.FftVisits;
         }
-        finderCache[node.Id] = counter;
-        return counter;
+        // Console.WriteLine($"Node {node.Id}");
+        finderCache[node.Id] = new NodeCheckPoint(
+            visits,
+            node.Id == "dac" ? (fftVisits > 0 ? fftVisits : visits) : dacVisits,
+            node.Id == "fft" ? (dacVisits > 0 ? dacVisits : visits) : fftVisits);
+        return finderCache[node.Id];
     }
 
     public override ulong SolvePart2()
     {
-        // foreach (var node in Nodes)
-        //     Console.WriteLine($"{node.Key} \n\tInputs:{string.Join(", ", node.Value.Inputs.Select(n => n.Id))} \n\tOutput:{string.Join(", ", node.Value.Outputs.Select(n => n.Id))}");
-        var queue = new Queue<Node>();
-        var outNode = Nodes["out"];
-        outNode.visits = 1;
-        queue.Enqueue(outNode);
-        while (queue.Count > 0)
-        {
-            var node = queue.Dequeue();
-            if (node.isChecked)
-                continue;
-            node.isChecked = true;
-            // Console.WriteLine($"{node.Id} {node.visits} fft={node.passedFft} dac={node.passedDac}");
-            if (node.Id == "fft")
-                node.passedFft = true;
-            if (node.Id == "dac")
-                node.passedDac = true;
-
-            foreach (var input in node.Inputs)
-            {
-                /* node and input have matching passed */
-                if (node.passedFft == input.passedFft && node.passedDac == input.passedDac)
-                {
-                    input.visits += node.visits;
-                }
-                /* node has more passed than input */
-                else if ((!input.passedFft && !input.passedDac && (node.passedDac || node.passedFft)) ||
-                         ((input.passedFft || input.passedDac) && input.passedDac != input.passedFft && node.passedDac && node.passedFft))
-                {
-                    input.visits = node.visits;
-                    input.passedFft = node.passedFft;
-                    input.passedDac = node.passedDac;
-                }
-                /* both node and parent have a different passed */
-                else if ((input.passedFft || input.passedDac) && (node.passedFft || node.passedDac) && node.passedDac != input.passedDac && node.passedFft != input.passedFft)
-                {
-                    input.visits += node.visits;
-                    input.passedFft = true;
-                    input.passedDac = true;
-                }
-                queue.Enqueue(input);
-            }
-        }
-        return Nodes["svr"].visits;
+        var counter = PathFinder(Nodes["svr"]);
+        // foreach (var node in finderCache)
+        // Console.WriteLine($"{node.Key}: {node.Value.Visits} fft={node.Value.FftVisits} dac={node.Value.DacVisits}");
+        return Math.Min(counter.DacVisits, counter.FftVisits);
     }
 
     internal override void ParseInput()
